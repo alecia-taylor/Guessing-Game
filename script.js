09d8da527e65fe269088760c4f61e1d20d11aaf8
A. Taylor <alecia.taylor0@gmail.com>
2025-04-14 00:10:55 +0000

    Add initial implementation of the Guessing Game with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let attempts = 10;
+
+function checkGuess() {
+  const inputElement = document.getElementById('guess');
+  const feedbackElement = document.getElementById('feedback');
+  let guess = inputElement.value;
+
+  attempts--; 
+
+  if (randomNumber == guess) {
+    feedbackElement.innerHTML = "Success!";
+    feedbackElement.style.color = "green";
+  } else if (guess < randomNumber) {
+    feedbackElement.innerHTML = `Too low! Try again. Attempts left: ${attempts}`;
+    feedbackElement.style.color = "red";
+  } else if (guess > randomNumber) {
+    feedbackElement.innerHTML = `Too high! Try again. Attempts left: ${attempts}`;
+    feedbackElement.style.color = "red";
+  }
+}
